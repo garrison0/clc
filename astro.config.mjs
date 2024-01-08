1c446312bbc2f282a38d1d52f5154c08743d8d73
Garrison McMullen <garrison@ofgskmfx.mynetworksettings.com>
2024-01-08 18:53:50 -0500

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
+import decapCmsOauth from "astro-decap-cms-oauth";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-naut-template.vercel.app/",
+  integrations: [icon(), decapCmsOauth()]
 });
---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+    {
+      title: 'Instagram',
+      icon: 'mdi:instagram',
+      url: 'https://www.instagram.com/juniperandpine.music',
+    },
+    {
+      title: 'YouTube',
+      icon: 'mdi:youtube',
+      url: 'https://www.youtube.com/@juniperandpine-music',
+    },
+    {
+      title: 'Bandcamp',
+      icon: 'mdi:music-box-multiple',
+      url: 'https://juniperandpine.bandcamp.com/'
+    }
+  ]
