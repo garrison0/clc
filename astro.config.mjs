a58a4c5a9c50147bb441562b3abee85687fa17d0
Garrison McMullen <garrison@ofgskmfx.mynetworksettings.com>
2024-01-08 21:33:32 -0500

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import icon from "astro-icon";
 import decapCmsOauth from "astro-decap-cms-oauth";
 
 import netlify from "@astrojs/netlify";
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://melodic-brioche-682b75.netlify.app/",
   integrations: [icon(), decapCmsOauth()],
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    plugins: [yaml()]
+  }
 });
