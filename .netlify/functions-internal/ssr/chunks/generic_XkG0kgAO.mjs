e036e57a2b01264a0437b29f579c3898a8b0fee9
Garrison McMullen <garrison@ofgskmfx.mynetworksettings.com>
2024-01-08 19:15:40 -0500

    new auth

--- .netlify/functions-internal/ssr/chunks/generic_XkG0kgAO.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_9JMb-VO9.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/pages/index_3hSIN9L5.mjs
+++
@@ -1,7 +0,0 @@
-import { a as authUrl } from './callback_wyknxn_y.mjs';
-
-const GET = ({ redirect }) => {
-  return redirect(authUrl);
-};
-
-export { GET };
