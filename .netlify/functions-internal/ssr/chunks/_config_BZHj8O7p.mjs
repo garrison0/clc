a50ef54e45f9c4f3529d9446dfc082f07fb10eff
garrison0 <mcmullengn@gmail.com>
2026-03-25 15:36:58 -0400

    attempt 2

--- .netlify/functions-internal/ssr/chunks/_config_BZHj8O7p.mjs
+++
@@ -1,6 +0,0 @@
-const clientId = process.env.OAUTH_GITHUB_CLIENT_ID;
-const clientSecret = process.env.OAUTH_GITHUB_CLIENT_SECRET;
-const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`;
-const tokenUrl = "https://github.com/login/oauth/access_token";
-
-export { clientId as a, authUrl as b, clientSecret as c, tokenUrl as t };
